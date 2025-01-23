8d4df2b19d65ca6499aea7caafa759836761fae8
Macbook pro <macbookpro@YourexBoy.local>
2025-01-23 05:58:49 -0800

    G

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,109 +1,135 @@
-import "./Login.css";
-import LoginImg from "../../assets/Login-img.gif";
-import SwiftLogo from "../../assets/Swift-Earn-Logo.png";
-import {CiMail} from "react-icons/ci";
-import {FiKey} from "react-icons/fi";
-import {useNavigate} from 'react-router-dom'
+import {FaInstagram, FaTelegram, FaTwitter} from "react-icons/fa";
+import {NavLink, useNavigate} from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import toast,{ Toaster } from 'react-hot-toast';
+import axios from "axios";
+import { userId } from "../../global/features";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { ClipLoader } from "react-spinners";
 
 const Login = () => {
 
-  const nav = useNavigate()
+const Nav = useNavigate()
+const dispatch = useDispatch()
+const [loading, setLoading] = useState(false)
+    
+  const User = z.object({
+    email: z.string().email({ message: 'Must be a valid email' }),
+    password: z.string()
+    .min(1,{message: "Password is required"})
+   .regex(
+      /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/,
+      {message: "Password must be 8 characters long, uppercase and special character (!@#$%^&*)."}
+   ),
+  });
 
-  const handleNavToDashboard = () =>{
-    nav('/dashboard')
-  }
+  const { register, handleSubmit, formState: { errors }, setError } = useForm({
+    resolver: zodResolver(User),
+  });
+
+  const Onsubmit = async (data, e) => {
+    console.log(data);
+    setLoading(true)
+    e.preventDefault(); 
+    const url = 'https://fsuth-back-ends.vercel.app/api/login'
+    const FormData ={
+      email: data.email,
+      password: data.password,
+    }
+     
+    await axios.post(url, FormData)
+    .then(response=>{
+      setLoading(false)
+       console.log("response:",response.data?._id);
+       dispatch(userId(response?.data?._id))
+       toast.success(response?.message)
+       Nav("/dashboard")
+    })
+    .catch(error =>{
+      setLoading(false)
+      toast.error(error?.response?.message)
+      console.log("error:",error)
+    })
+  };
+  const currentYear = new Date().getFullYear();
 
     return (
-        <>
-            <div className="LoginBody">
-                <div className="LoginContainer">
-                    <div className="LoginRight">
-                        <div className="LoginTitle">
-                            <h1>Welcome Back!</h1>
-                            <p>
-                                To keep you connected, please login with your
-                                personal info
-                            </p>
-                        </div>
-                        <div className="LoginImg">
-                            <img src={LoginImg} alt="Login Image..." />
-                        </div>
-                    </div>
-                    <div className="LoginLeft">
-                        <div className="LoginLeftWrapper">
-                            <div className="LoginLeftImgDiv">
-                                <img src={SwiftLogo} alt="Swift Earn Logo" />
-                            </div>
-                            <div className="LoginLeftInputsDiv">
-                                <div className="LoginLeftInputErrorDiv">
-                                    <ul>
-                                        <li>
-                                            These credentials do not match our
-                                            records
-                                        </li>
-                                    </ul>
-                                    <span>
-                                        <i
-                                            className="fa-solid fa-xmark"
-                                            
-                                        ></i>
-                                    </span>
-                                </div>
-                                <h2>Log in</h2>
-                                <div className="LoginLeftEmailDiv">
-                                    <label htmlFor="">
-                                        Your Email <span>*</span>
-                                    </label>
-                                    <div className="LoginLeftEmailDivInputBox">
-                                        <span>
-                                            <CiMail className="CiMail"/>
-                                        </span>
-                                        <input
-                                            type="email"
-                                            placeholder="name@example.com"
-                                        />
-                                    </div>
-                                </div>
-                                <div className="LoginLeftPasswordDiv">
-                                    <label htmlFor="">
-                                        Password <span>*</span>
-                                    </label>
-                                    <div className="LoginLeftPasswordDivInputBox">
-                                        <span>
-                                            <FiKey className="FiKey"/>
-                                        </span>
-                                        <input
-                                            type="password"
-                                            placeholder="Enter Password"
-                                        />
-                                    </div>
-                                </div>
-                                <div className="LoginLeftControlDiv">
-                                    <div className="LoginLeftControlDivRememberDiv">
-                                        <input type="checkbox" id="remember" />
-                                        <p>Remember me</p>
-                                    </div>
-                                    <p>Forget password?</p>
-                                </div>
-                                <div className="LoginLeftBtnDiv">
-                                    <button onClick={handleNavToDashboard}>Sign in</button>
-                                </div>
-                            </div>
-                            <div className="LoginLeftInfo">
-                                <p className="LoginLeftInfoSignUp">
-                                    Don&#39;t have an account?{" "}
-                                    <span>Sign Up</span>
-                                </p>
-                                <p className="LoginLeftInfoRights">
-                                    © Copyright 2023 &nbsp; Swift Earn Trade
-                                    &nbsp; All Rights Reserved.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+        <div className="w-full h-screen">
+            <div className="w-full h-12 bg-[#f8f8f8] phone:hidden px-48 flex items-center justify-between">
+                <div className="w-max flex items-center gap-4">
+                    <FaTwitter />
+                    <FaTelegram />
+                    <FaInstagram />
+                </div>
+                <div className="w-max flex items-center gap-5 text-sm">
+                    <div className="w-max h-max cursor-pointer">Help</div>
+                    <div className="w-max h-max cursor-pointer">Support</div>
+                    <div className="w-max h-max cursor-pointer">Login</div>
+                    <div className="w-max h-max cursor-pointer">Register</div>
                 </div>
             </div>
-        </>
+            <div className="w-full h-[calc(100%-6.5rem)] phone:h-[calc(100%-6rem)] bg-[#ffffff] flex items-center justify-center flex-col gap-5">
+            <p className="text-xl text-[#5d3891] font-semibold">
+                    Login your account
+                </p>
+                <form onSubmit={handleSubmit(Onsubmit)} className="w-max phone:w-full phone:px-4 h-max flex flex-col items-center gap-5">
+                    <input
+                        type="text"
+                        name=""
+                        id=""
+                        className="w-[33rem] phone:w-full h-12 rounded border border-gray-100 bg-[#f8f8f8] outline-none pl-4"
+                        placeholder="Email *"
+                        required
+                        {...register("email")}
+                    />
+              {errors?.email && <span style={{ color: "red" }}>{errors.email.message}</span>}
+          <input
+                        type="password"
+                        name=""
+                        id=""
+                        className="w-[33rem] phone:w-full h-12 rounded border border-gray-100 bg-[#f8f8f8] outline-none pl-4"
+                        placeholder="password *"
+                        required
+                        {...register("password")}
+                    />
+                     {errors?.password && <span style={{ color: "red" }}>{errors.password.message}</span>}
+                    <button
+                        type="submit"
+                        // onClick={() => Nav('/dashboard')}
+                        className="w-40 h-12 rounded bg-[#a286f4] text-white text-sm font-bold transition-all duration-500 hover:bg-white hover:border-2 hover:text-[#a286f4] hover:border-[#a286f4]"
+                    >
+                         { loading ? <ClipLoader color='white' className="hover:bg-#a286f4" /> :
+                               " LOG IN" 
+                               } 
+                       
+                    </button>
+                    <div className="w-max phone:w-full phone:justify-between phone:gap-0 h-max flex gap-80 text-sm text-[#a286f4]">
+                        <div className="w-max h-max cursor-pointer" onClick={() => Nav('forgotten-password')}>
+                            Forgot Password?
+                        </div>
+                        <NavLink to={"/register"}>
+                            <div className="w-max h-max cursor-pointer">
+                                Create Account
+                            </div>
+                        </NavLink>
+                    </div>
+                </form>
+            </div>
+            <div className="w-full phone:h-24 phone:gap-3 phone:flex-col phone:justify-center  phone:py-4 h-14 text-white px-48 flex items-center justify-between bg-[#0e1120]">
+                <div className="w-max flex items-center gap-4">
+                <p className="flex gap-5 items-center text-white">&copy;  Copy Rights {currentYear}. All Rights Reserved fininvestohub</p>
+                </div>
+                <div className="w-max flex items-center gap-5 ">
+                    <FaTwitter />
+                    <FaTelegram />
+                    <FaInstagram />
+                </div>
+            </div>
+            <Toaster position="top-center" />
+            </div>
     );
 };
 
